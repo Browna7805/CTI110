896bc6a261ed9fd7ada158fe5575f94e3b25720c
Browna7805 <47749762+Browna7805@users.noreply.github.com>
2019-02-20 12:01:06 -0500

    Add files via upload

---
+++ P2HW1_PoundsKilograms_AntwonBrown.py
@@ -0,0 +1,22 @@
+#Converting Pounds to Kilograms
+#2/12/19
+#CTI-110 P2HW1 - Pounds to Kilograms Converter
+#Antwon Brown
+
+
+
+
+# Input pounds
+# Calculate formula     
+# Display kilograms
+
+
+#User Inputs Pound
+pounds = int(input('Enter pounds here: '))
+
+
+#Caculate the amount of Pounds lbs/2.2046
+kilograms = pounds / 2.2046
+
+#Displays Kilograms after Conversion
+print('Total kilograms',kilograms,'Kg')
